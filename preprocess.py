6e5fa1f11bd6729860fcdb903cd2ab1f19617c7f
Marine Prost <marine@speechmorphing.com>
2024-09-24 16:10:57 +0200

    # 2 Pull-ups are now caught correctly

--- preprocess.py
+++ preprocess.py
@@ -3,21 +3,7 @@ import json
 from tqdm import tqdm
 from collections import defaultdict
 
-# Function to parse workout data from a text block
-def parse_workout_data(workout_text):
-    data = defaultdict(list)
-    lines = workout_text.strip().split("\n")
-    current_date = None
-    
-    for line in tqdm(lines):
-        if line: # Only processes if the line is not empty string
-            # Match date pattern
-            date_match = re.match(r"(\d{2}/\d{2}/\d{2,4})", line)
-            if date_match:
-                current_date = date_match.group(1)
-                continue
-
-            
+def preprocessing(line):
             # Searching for badly split exercice and sets_reps
             pattern = r"^(?P<exercice>.+?)(?P<sets_reps>([0-9]{0,4}(,|.)?[0-9]{0,4}|\w{0,4})x[0-9]{1,4}(,|.)?[0-9]{0,4})"
             split_error = re.search(pattern, line)
@@ -26,6 +12,18 @@ def parse_workout_data(workout_text):
                 if not "-" in split_result:
                     # print(f"no - detected in {split_result}")
                     line = re.sub(pattern, r"\g<exercice> - \g<sets_reps>", line)
+
+    
+    # Searching for badly split exercice and sets_reps
+    pattern = r"^(?P<exercice>.+?)(?P<sets_reps>([0-9]{0,4}(,|.)?[0-9]{0,4}|\w{0,4})x[0-9]{1,4}(,|.)?[0-9]{0,4})"
+    split_error = re.search(pattern, line)
+    if split_error:
+        split_result = split_error.group('exercice')
+        if not "-" in split_result:
+            # print(f"no - detected in {split_result}")
+            line = re.sub(pattern, r"\g<exercice> - \g<sets_reps>", line)
+            line = line.replace("  ", " ")
+
 
             # Searches any dropsets
             drop_sets = re.search(r"(?P<weight>[0-9]+,?[0-9]*)x(?P<set>[0-9]+,?[0-9]*)(?P<drop>/[0-9]+,?[0-9]*)+", line)
@@ -48,6 +46,31 @@ def parse_workout_data(workout_text):
                     
                     new = re.sub(rec_pattern, r"\g<weight>x\g<drop> - \g<weight>x", new)
 
+    return line
+
+
+
+# Function to parse workout data from a text block
+def parse_workout_data(workout_text):
+    data = defaultdict(list)
+    lines = workout_text.strip().split("\n")
+
+
+
+    current_date = None
+    
+    for line in tqdm(lines):
+        if line: # Only processes if the line is not empty string
+            line = line.replace("+", " + ")
+            line = line.replace("-", " - ")
+            line = line.replace("  ", " ")
+            # Match date pattern
+            date_match = re.match(r"(\d{2}/\d{2}/\d{2,4})", line)
+            if date_match:
+                current_date = date_match.group(1)
+                continue
+            line = preprocessing(line)
+
 
 
             # Parse exercises, weights, reps, sets
@@ -58,19 +81,31 @@ def parse_workout_data(workout_text):
                 
                 # Separate the sets and reps, e.g., '30x8 - 52.5x5' => [(30, 8), (52.5, 5)]
                 sets_reps_list = re.findall(r"(\d+(?:,\d+)?(?:\.\d+)?(?:x\d+))", sets_reps)
+
+                # If no number found, it's probably for Pull-ups with letters instead of numbers
+                if not sets_reps_list:
+                    sets_reps_list = re.findall(r"\b[A-Z]+[A-Z0-9]*x\d+(?:[.,]\d+)?", sets_reps)
                 
                 parsed_sets = []
                 for set_rep in sets_reps_list:
                     # Split weight and reps e.g. '30x8' => (30, 8)
                     weight, reps = set_rep.split("x")
+
+                    # replace comas for French numbers.
                     if "," in weight:
                         weight = weight.replace(",", ".")
-                    parsed_sets.append({"weight": float(weight), "reps": int(reps)})
+
+                    # Convert reps to int
+                    parsed_sets.append({"weight": weight, "reps": reps})
+
+                # Output the parsed sets
+                print(parsed_sets)
                 
                 # Append data to the structured format
                 data[current_date].append({
                     "exercise": exercise,
-                    "sets": parsed_sets
+                    "sets": parsed_sets,
+                    "line": line
                 })
     
     return data
